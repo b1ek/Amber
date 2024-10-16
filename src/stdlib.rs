1bfc1a602d45bcb83a7a776bc4da8c99a9e54f2b
b1ek <me@blek.codes>
2024-10-16 14:45:37 +1000

    feat: allow multi level stdlib imports

--- src/stdlib.rs
+++ src/stdlib.rs
@@ -4,8 +4,8 @@ pub const STDLIB: Dir = include_dir!("src/std");
 pub fn resolve<T: Into<String>>(path: T) -> Option<String> {
     let path = path.into();
 
-    if let Some(module) = STDLIB.get_file(path + ".ab") {
-        let module = module.contents_utf8().unwrap().to_string();
+    if let Some(module) = STDLIB.get_entry(path + ".ab") {
+        let module = module.as_file()?.contents_utf8()?.to_string();
         Some(module)
     } else {
         None
